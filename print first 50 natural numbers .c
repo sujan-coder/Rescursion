e5480aeae7fee13bb3357c4ecf6091ec4906dc6f
SUJAN K K <62798625+sujan-coder@users.noreply.github.com>
2023-10-03 15:34:38 +0530

    Create  print first 50 natural numbers .c
    O/P:                                                                                                            
     The natural numbers are : 1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  
    24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39  40  41  42  43  44  45  46  47  48  49  50

---
+++ print first 50 natural numbers .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int  numPrint(int);
+int main()
+{
+    int n = 1;
+	printf(" The natural numbers are :");
+    numPrint(n);
+    printf("\n");
+    return 0;
+}
+int numPrint(int n)
+{
+    if(n<=50)
+    {
+         printf(" %d ",n);
+         numPrint(n+1);
+    }
+}
