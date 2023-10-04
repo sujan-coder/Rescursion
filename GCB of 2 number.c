a0636ea565c10899fcd5d68d90a30abd1007097a
SUJAN K K <62798625+sujan-coder@users.noreply.github.com>
2023-10-04 15:31:04 +0530

    Create GCB of 2 number.c
    O/P:
    Enter the numbers
    30
    45
    GCD :15

---
+++ GCB of 2 number.c
@@ -0,0 +1,16 @@
+// Write a program in C to find the GCD of two numbers using recursion
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1,n2;
+    printf("Enter the numbers\n");
+    scanf("%d %d",&n1,&n2);
+    printf("GCD :%d",gcd(n1,n2));
+    return 0;
+}
+int gcd(int n1,int n2)
+{
+    if(n1
+}
